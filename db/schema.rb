b8157cf58b279a86f637f801222282c74fb45fc4
Mat <mvello66@gmail.com>
2021-03-11 22:30:22 -0500

    adds working has many through associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_031327) do
+ActiveRecord::Schema.define(version: 2021_03_12_032155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_post_tags", force: :cascade do |t|
+    t.integer "blog_post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
